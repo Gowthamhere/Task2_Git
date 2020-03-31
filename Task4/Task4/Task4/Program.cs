fd36406fdbd0fedadfdd3bd3598daa590014fd66
Gowtham <lazycoder@yahoo.com>
2020-03-31 05:59:15 +0300

    Initial Commit matrix

---
+++ Task4/Task4/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task4
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            Random rand = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rand.Next(0, 10);
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = i+1; j < array.GetLength(1); j++)
+                {
+                    if (j > i)
+                    {
+                        array[i, j] = 0;
+                       
+                    }                    
+                }                
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
